3d79430fab4c43ce1da8074adc6520391c8c91ae
Riku Tiitta <riku.tiitta@edu.karelia.fi>
2018-11-16 13:02:25 +0200

    Pelikoodi siistitty ja kommentoitu. Räjähdysanimaatio korjattu.

--- gamelogic.js
+++ gamelogic.js
@@ -1,665 +1,265 @@
       
 
+//boolean array for all keycodes to detect keys
 var keys = [];
 	  
-
-
-var voitto = "Error";
+//if this is true game will be drawn and run normally
 var game_running = true;
 
-class BasePlayer
-{
-	constructor(x,y)
-	{
-		this.x = x;
-		this.y = y;
-		this.width = 20;
-		this.height = 50;
-		this.angle = 0;
-		this.score = 100;
-		this.role = 'undefined';
-	}
-
-	setPosition(x,y)
-	{		
-		this.x = x;
-		this.y = y;
-	}
-	
-	setAngle(angl)
-	{
-		this.angle = angl;
-	}
-
-	get getAngle()
-	{
-		return this.angle;
-	}
-
-	get getX()
-	{
-		return this.x;
-	}
-
-	
-	get getY()
-	{
-		return this.y;
-	}
-
-	get getWidth()
-	{
-		return this.width;
-	}
-
-	get getHeight()
-	{
-		return this.height;
-	}
-
-	setRole(new_role)
-	{
-		this.role = new_role;
-	}
-
-	get getRole()
-	{
-		return this.role;
-	}
-
-	get getScore()
-	{
-		return this.score;
-	}
-
-	setScore(score)
-	{
-		this.score = score;
-	}
-	
-}
-
-class LocalPlayer extends BasePlayer
-{
-	constructor(x,y)
-	{
-		super(x,y);
-		this.velocity_x = 0;
-		this.velocity_y = 0;
-		this.last_x = 0;
-		this.last_y = 0;
-		this.last_angle = 0;
-		this.score = 100;
-	}
-
-	handleBounds() {
-		var canvas = document.getElementById('canvas');
-		//prevent out of bounds values
-		if ((this.x + this.width) >= canvas.width) {
-			this.x = (canvas.width - this.width) - 10;
-			this.velocity_x = 0;
-
-			//went out from right
-			//release the key
-			keys[39] = false;
-			//invert direction
-			this.angle = -this.angle;
-		}
-		else if ((this.x) <= 0) {
-			this.x = 10;
-			this.velocity_x = -this.velocity_x;
-			//went out from left
-			//release the key
-			keys[37] = false;
-			//invert direction
-			this.angle = -this.angle;
-
-		}
-
-		if ((this.y) <= 0) {
-			this.y = (10);
-			this.velocity_y = -this.velocity_y;
-
-			//went out from top
-			//release the key
-			keys[38] = false;
-			//invert direction
-			this.angle = 180;
-		}
-		else if ((this.y + this.height) >= canvas.height) {
-			this.y = (canvas.height - this.height) - 10;
-			this.velocity_y = 0;
-			//went out from bottom
-			//release the key
-			keys[40] = false;
-			//invert direction
-			this.angle = 0;
-		}
-	}
-
-	get getVelocityX()
-	{
-		return this.velocity_x;
-	}
-
-	get getVelocityY()
-	{
-		return this.velocity_y;
-	}
-
-	get getLastX()
-	{
-		return this.last_x;
-	}
-
-	get getLastY()
-	{
-		return this.last_Y;
-	}
-
-
-	setLastPosition(x,y)
-	{
-		this.last_x = x;
-		this.last_y = y;
-	}
-
-	addVelocity(x,y)
-	{
-		this.velocity_x += x;
-		this.velocity_y += y;
-	}
-
-
-
-	draw()
-	{
-					
-		var canvas = document.getElementById('canvas');			
-		var ctx = canvas.getContext('2d');
-
-			
-		ctx.fillStyle = 'rgba(255, 255, 23)';
-		ctx.font = '18px tahoma';
-		ctx.textAlign = 'center';		
-		var center_x = (this.x + (this.width / 2));
-		
-		ctx.fillText('(you)', center_x, this.y - 20);
-
-
-		ctx.save();
-		ctx.fillStyle = 'rgb(255, 255, 0)';
-
-		ctx.translate(this.x+this.width/2,this.y+this.height/2);
-
-		ctx.rotate(this.angle*Math.PI/180);
-
-		if(this.role == 'poliisi')
-		{
-			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-		}
-		else
-		{
-			ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
-
-		}
-
-		//base draw
-		ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
-
-		ctx.fillStyle = 'rgb(200, 200, 200)';
-
-		//show which way its facing
-		ctx.fillRect(-this.width/2, -this.height/2, this.width, 5);
-
-		ctx.restore();
-
-	}
-
-	setLastAngle(angle)
-	{
-		this.last_angle = angle;
-	}
-
-	checkSendUpdate() {
-
-		//convert to int to prevent unnecessary updates saving network bandwidth
-		var x_int = Math.floor(this.x);
-		var y_int = Math.floor(this.y);
-		var angle_int = Math.floor(this.angle);
-
-		var lastx_int = Math.floor(this.last_x);
-		var lasty_int = Math.floor(this.last_y);
-		var last_angle_int = Math.floor(this.last_angle);
-
-
-		//check comparing previous values if we need to send update to server
-		if (x_int != lastx_int || y_int != lasty_int || angle_int != last_angle_int) {
-
-			socket.emit('position update', this.x, this.y, this.angle);			
-			this.setLastPosition(x_int,y_int);
-			this.setLastAngle(angle_int);
-		}
-	}
-
-	setPosition(x,y)
-	{	
-		super.setPosition(x,y);
-		this.handleBounds();
-		this.checkSendUpdate();
-	}
-
-
-}
-
-class Enemy extends BasePlayer
-{
-	constructor(x,y)
-	{
-		super(x,y);
-	}
-
-	draw()
-	{
-					
-		var canvas = document.getElementById('canvas');			
-		var ctx = canvas.getContext('2d');
-
-		ctx.save();
-		ctx.fillStyle = 'rgb(255, 255, 0)';
-
-		ctx.translate(this.x+this.width/2,this.y+this.height/2);
-
-		ctx.rotate(this.angle*Math.PI/180);
-
-		if(this.role == 'poliisi')
-		{
-			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-		}
-		else
-		{
-			ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
-
-		}
-		//base draw
-		ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
-
-		ctx.fillStyle = 'rgb(200, 200, 200)';
-
-		//show which way its facing
-		ctx.fillRect(-this.width/2, -this.height/2, this.width, 5);
-
-		ctx.restore();
-
-	}
-
-}
-
-//connect to node server
-var socket =  io.connect('http://localhost:3000');
-
+//global enemy
 var enemy = 0;
+//global localplayer
 var localplayer = 0;
 
-socket.on('player joined', (pos_x,pos_y) => {
+//handle localplayer movement and projectiles on key presses
+function handleLocalPlayerKeys()
+{
+	//speed we update our origin
+	var speed = 10;
+	var pos_x = localplayer.getX;
+	var pos_y = localplayer.getY;
+	var angle = localplayer.getAngle;
 
-	enemy = new Enemy(pos_x,pos_y);
+	//down arrow key
+	if (keys[40]) {
+		pos_y += speed;
+		angle = 180;
+	}
+	//up arrow key
+	else if (keys[38]) {
+		pos_y -= speed;
+		angle = 0;
+	}
+	//right arrow key
+	else if (keys[39]) {
+		pos_x += speed;
+		angle = 90;
 
-	if(localplayer.getRole == 'poliisi')
-		enemy.setRole('rosvo')
-	else
-		enemy.setRole('poliisi')
-	
-	console.log("enemy joined");
+	}
+	//left arrow key
+	else if (keys[37]) {
+		pos_x -= speed;
+		angle = -90;
+	}
 
-});
-
-
-socket.on('position update', (pos_x,pos_y,angle) => {
-
-	enemy.setPosition(pos_x,pos_y);
-	enemy.setAngle(angle);
-
-});
-
-
-
-socket.on('get start info', (pos_x,pos_y,role) => {
-
-	localplayer = new LocalPlayer(pos_x,pos_y);
-	
-	localplayer.setRole(role);
+	//set new position and angle
+	localplayer.setAngle(angle);			
 	localplayer.setPosition(pos_x,pos_y);
-
-	console.log("got start info");
-});
-
-var projectiles = [];
-
-class projectile{
-	constructor(x,y)
-	{
-		this.x = x;
-		this.y = y;
-		this.height = 2;
-		this.width = 5;
-		this.angle = 0;
-		this.created_locally = false;
-	}
-
-	setPosition(x,y)
-	{		
-		this.x = x;
-		this.y = y;
-	}
-
-	setCreatedLocally()
-	{
-		this.created_locally = true;
-	}
-
-	setAngle(angl)
-	{
-		this.angle = angl;
-	}
-
-	get getAngle()
-	{
-		return this.angle;
-	}
-
-	get getX()
-	{
-		return this.x;
-	}
-
 	
-	get getY()
-	{
-		return this.y;
+	//spacebar
+	if(keys[32])
+	{			
+		//create new missile in 
+		var missile = new projectile(localplayer.getX,localplayer.getY);
+
+		//mark this missile as created by localplayer so it's not considered networked
+		missile.setCreatedLocally();
+		missile.setAngle(angle);
+		projectiles.push(missile);
+
+		//send server information that we fired a missile
+		socket.emit('create projectile',missile.getX,missile.getY,angle);
+		//set the spacebar value as false so we only fire once per event
+		keys[32] = false;
 	}
+}
+//loop of all active projectiles move them, draw them and remove them if needed
+function handleMissilesArray() {
+	//movement speed of missile
+	var projectile_speed = 4;
+	for (i = 0; i < projectiles.length; i++) {
+		var missile = projectiles[i];
 
-	get getWidth()
-	{
-		return this.width;
-	}
-
-	get getHeight()
-	{
-		return this.height;
-	}
-
-	get createdLocally()
-	{
-		return this.created_locally;
-	}
-
-
-	draw()
-	{
-
-		var canvas = document.getElementById('canvas');			
-		var ctx = canvas.getContext('2d');
-
-
-		var missle_x = this.getX;
-		var missle_y = this.getY;
-
-		if(this.createdLocally)
-		{
-			if(localplayer.getRole == 'poliisi')
-			{
-				ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-			}
-			else
-			{
-				ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
-			}
+		//move missile in the direction its facing when created
+		if (missile.getAngle == 0) {
+			var pos_y = missile.getY - projectile_speed;
+			missile.setPosition(missile.getX, pos_y);
 		}
-		else
-		{
-			if(localplayer.getRole == 'rosvo')
-			{
-				ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-			}
-			else
-			{
-				ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
-			}
+		else if (missile.getAngle == 180) {
+			var pos_y = missile.getY + projectile_speed;
+			missile.setPosition(missile.getX, pos_y);
 
 		}
-		ctx.fillRect(missle_x, missle_y, this.getWidth, this.getHeight);
+		else if (missile.getAngle == 90) {
+			var pos_x = missile.getX + projectile_speed;
+			missile.setPosition(pos_x, missile.getY);
+		}
+		else if (missile.getAngle == -90) {
+			var pos_x = missile.getX - projectile_speed;
+			missile.setPosition(pos_x, missile.getY);
+		}
 
+		//draw the missile
+		missile.drawProjectile();
+
+
+		var missile_x = missile.getX;
+		var missile_y = missile.getY;
+
+		//how many scorepoints will be given and taken on impact
+		var score_step = 5;
+		//check player collision
+		if (missile.createdLocally) {
+			var enemy_x = enemy.getX;
+			var enemy_y = enemy.getY;
+
+			//checks based on missile x, y, width, height and enemy x, y, width, height
+			if (missile_x < enemy_x + enemy.getWidth &&
+				missile_x + missile.getWidth > enemy_x &&
+				missile_y < enemy_y + enemy.getHeight &&
+				missile.getHeight + missile_y > enemy_y) {
+				var score = enemy.getScore;
+				var loc_score = localplayer.getScore;
+
+				//add 5 to localplayer score
+				localplayer.setScore(loc_score + score_step);
+				//remove 5 from enemy score
+				enemy.setScore(score - score_step);
+
+				//remove projectile from array
+				projectiles.splice(i, 1);
+
+			}
+		}
+		else {
+
+			var localplayer_x = localplayer.getX;
+			var localplayer_y = localplayer.getY;
+
+			//checks based on missile x, y, width, height and enemy x, y, width, height
+			if (missile_x < localplayer_x + localplayer.getWidth &&
+				missile_x + missile.getWidth > localplayer_x &&
+				missile_y < localplayer_y + localplayer.getHeight &&
+				missile.getHeight + missile_y > localplayer_y) {
+				var score = localplayer.getScore;
+
+				//remove 5 from our score
+				localplayer.setScore(score - score_step);
+
+				var enemy_score = enemy.getScore;		
+				//add 5 to enemy score
+				enemy.setScore(enemy_score + score_step);
+
+				//remove projectile from array
+				projectiles.splice(i, 1);
+
+			}
+		}
+
+
+		//if missile goes out of bounds remove it from projectiles list
+		//goes out of right edge
+		if ((missile_x + missile.getWidth) >= canvas.width) {
+			projectiles.splice(i, 1);
+		}
+		//goes out of left edge
+		else if ((missile_x) <= 0) {
+			projectiles.splice(i, 1);
+		}
+		//goes out of top edge
+		if ((missile_y) <= 0) {
+			projectiles.splice(i, 1);
+		}
+		//goes out of bottom edge
+		else if ((missile_y + missile.getHeight) >= canvas.height) {
+			projectiles.splice(i, 1);
+		}
 	}
 }
 
-socket.on('create projectile', (pos_x,pos_y,angle) => {
+//MAIN FUNCTION CALLED 60 TIMES A SECOND
+function main() {
+	var canvas = document.getElementById('canvas');
+	if (canvas.getContext) {
 
-	var missle = new projectile(pos_x,pos_y);
-
-	missle.setAngle(angle);
-	projectiles.push(missle);
-   
-});
-
+		//loop for next frame
+		requestAnimationFrame(main);
 
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-		
-		
-		requestAnimationFrame(draw);
-			
 		var ctx = canvas.getContext('2d');
+
+		//clear canvas from previous drawing
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		//background
+		//draw background
 		ctx.fillStyle = 'rgb(53, 53, 53,0.5)';
-		ctx.fillRect(0,0,canvas.width, canvas.height);
-		
-		//movement code
-		var speed = 10;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+		//if game is running. default is true
 		if (game_running) {
-			if(localplayer != 0)
-			{
-			var pos_x = localplayer.getX;
-			var pos_y = localplayer.getY;
-			var angle = localplayer.getAngle;
-			if (keys[40]) {
-				pos_y += speed;
-				angle = 180;
-			}
-			else if (keys[38]) {
-				pos_y -= speed;
-				angle = 0;
-			}
-			else if (keys[39]) {
-				pos_x += speed;
-				angle = 90;
 
+			//only if localplayer has been created we handle the movement code
+			if (localplayer != 0) {
+				handleLocalPlayerKeys();
 			}
-			else if (keys[37]) {
-				pos_x -= speed;
-				angle = -90;
-			}
-
-			
-			localplayer.setAngle(angle);			
-			localplayer.setPosition(pos_x,pos_y);
-			
-			if(keys[32])
-			{			
-				var missle = new projectile(localplayer.getX,localplayer.getY);
-
-				missle.setCreatedLocally();
-				missle.setAngle(angle);
-				projectiles.push(missle);
-
-				socket.emit('create projectile',missle.getX,missle.getY,angle);
-				keys[32] = false;
-			}
-		}
-
 
 			//top text drawn at center on x axis
 			ctx.fillStyle = 'rgba(255, 255, 23)';
 			ctx.font = '28px tahoma';
 			ctx.textAlign = 'center';
-			
-			var center_x = (canvas.width / 2);		
+			var center_x = (canvas.width / 2);
 			ctx.fillText('rosvo & poliisi', center_x, 50);
-
 
 
 			//scores
 			ctx.fillStyle = 'rgba(80, 60, 240)';
 			ctx.font = '18px tahoma';
 			ctx.textAlign = 'right';
-			
-			var center_x = (canvas.width) - 5;		
-			if(localplayer != 0)
-			{
+			var center_x = (canvas.width) - 5;
+			//check localplayer has been created
+			if (localplayer != 0) {
 				var score = localplayer.getScore;
 				var loc_text = "You " + String(score);
-				if(score <= 0)
+				if (score <= 0)
 					loc_text = 'Dead';
 				ctx.fillText(loc_text, center_x, 50);
 			}
 
-			if(enemy != 0)
-			{
+			//check enemy has been created
+			if (enemy != 0) {
 				var score = enemy.getScore;
 				var loc_text = "Enemy " + String(score);
-				if(score <= 0)
+				if (score <= 0)
 					loc_text = 'Dead';
 				ctx.fillText(loc_text, center_x, 80);
 			}
 
-			
 
 
-			if(localplayer != 0)
-			{
-				localplayer.draw();
+			//draw localplayer
+			if (localplayer != 0) {
+				localplayer.drawLocalPlayer();
 			}
 
-			if(enemy != 0)
-			{
-				enemy.draw();
+			//draw enemy
+			if (enemy != 0) {
+				enemy.drawEnemy();
 			}
 
-
-			var projectile_speed = 4;
-			for (i = 0; i < projectiles.length; i++) {
-				var missle = projectiles[i];
-
-				//movement direction of missle
-				if (missle.getAngle == 0) {
-					var pos_y = missle.getY - projectile_speed;
-					missle.setPosition(missle.getX, pos_y);
-				}
-				else if (missle.getAngle == 180) {
-					var pos_y = missle.getY + projectile_speed;
-					missle.setPosition(missle.getX, pos_y);
-
-				}
-				else if (missle.getAngle == 90) {
-					var pos_x = missle.getX + projectile_speed;
-					missle.setPosition(pos_x, missle.getY);
-				}
-				else if (missle.getAngle == -90) {
-					var pos_x = missle.getX - projectile_speed;
-					missle.setPosition(pos_x, missle.getY);
-				}
-
-
-				missle.draw();
-
-
-				var missle_x = missle.getX;
-				var missle_y = missle.getY;
-
-				//check player collision
-				if (missle.createdLocally) {
-					var enemy_x = enemy.getX;
-					var enemy_y = enemy.getY;
-
-					if (missle_x < enemy_x + enemy.getWidth &&
-						missle_x + missle.getWidth > enemy_x &&
-						missle_y < enemy_y + enemy.getHeight &&
-						missle.getHeight + missle_y > enemy_y) {
-						var score = enemy.getScore;
-						var loc_score = localplayer.getScore;
-
-						localplayer.setScore(loc_score + 5);
-
-						enemy.setScore(score - 5);
-						projectiles.splice(i, 1);
-
-					}
-				}
-				else {
-
-					var enemy_x = localplayer.getX;
-					var enemy_y = localplayer.getY;
-
-					if (missle_x < enemy_x + enemy.getWidth &&
-						missle_x + missle.getWidth > enemy_x &&
-						missle_y < enemy_y + enemy.getHeight &&
-						missle.getHeight + missle_y > enemy_y) {
-						var score = localplayer.getScore;
-
-						localplayer.setScore(score - 5);
-
-						var enemy_score = enemy.getScore;
-
-						enemy.setScore(enemy_score + 5);
-
-						projectiles.splice(i, 1);
-
-					}
-				}
-
-
-				//if missle goes out of bounds remove it from projectiles list
-				if ((missle_x + missle.getWidth) >= canvas.width) {
-					projectiles.splice(i, 1);
-				}
-				else if ((missle_x) <= 0) {
-					projectiles.splice(i, 1);
-
-				}
-
-				if ((missle_y) <= 0) {
-					projectiles.splice(i, 1);
-
-				}
-				else if ((missle_y + missle.getHeight) >= canvas.height) {
-					projectiles.splice(i, 1);
-
-				}
-			}
-			
+			//loop every missile in the array
+			handleMissilesArray();
+		}
+		//if game no longer running do something here
+		else {
 
 		}
-		else{
 
-		}
-		
-    }
+	}
 }
 
+//binder for key events and set the boolean value in array to true/false according to the event and keycode
 document.body.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
+	keys[e.keyCode] = true;
 });
 document.body.addEventListener("keyup", function (e) {
-    keys[e.keyCode] = false;
+	keys[e.keyCode] = false;
 });
-	  
 
+
+//emit join message to server
 socket.emit('client join');
 
+//start the drawing loop
+main();
 
-draw();
-	  
---
+++ player.js
@@ -0,0 +1,337 @@
+
+
+class BasePlayer
+{
+	constructor(x,y)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = 20;
+		this.height = 50;
+		this.angle = 0;
+		this.score = 100;
+		this.role = 'undefined';
+	}
+
+	setPosition(x,y)
+	{		
+		this.x = x;
+		this.y = y;
+	}
+	
+	setAngle(angl)
+	{
+		this.angle = angl;
+	}
+
+	get getAngle()
+	{
+		return this.angle;
+	}
+
+	get getX()
+	{
+		return this.x;
+	}
+
+	
+	get getY()
+	{
+		return this.y;
+	}
+
+	get getWidth()
+	{
+		return this.width;
+	}
+
+	get getHeight()
+	{
+		return this.height;
+	}
+
+	setRole(new_role)
+	{
+		this.role = new_role;
+	}
+
+	get getRole()
+	{
+		return this.role;
+	}
+
+	get getScore()
+	{
+		return this.score;
+	}
+
+	setScore(score)
+	{
+		this.score = score;
+	}
+	
+}
+var img = new Image();
+img.src = 'exp2.png';
+//frame counter
+var counter = 0;
+//current iteration step of image
+var vertical_index = 0;
+//movement direction on the image
+var horizontal_index = 0;
+class LocalPlayer extends BasePlayer
+{
+	constructor(x,y)
+	{
+		//this will call the BasePlayer class constructor to setup default values
+		super(x,y);
+		//velocity is just a placeholder if and until we make more fluent movement code
+		this.velocity_x = 0;
+		this.velocity_y = 0;
+		this.last_x = 0;
+		this.last_y = 0;
+		this.last_angle = 0;
+		//we can access BasePlayer variables here too due class derivation
+		this.score = 100;
+	}
+    //make sure player cant go out of playfield
+	handleBounds() {
+		var canvas = document.getElementById('canvas');
+		//prevent out of bounds values
+		if ((this.x + this.width) >= canvas.width) {
+			this.x = (canvas.width - this.width) - 10;
+			this.velocity_x = 0;
+
+			//went out from right
+			//release the key
+			keys[39] = false;
+			//invert direction
+			this.angle = -this.angle;
+		}
+		else if ((this.x) <= 0) {
+			this.x = 10;
+			this.velocity_x = -this.velocity_x;
+			//went out from left
+			//release the key
+			keys[37] = false;
+			//invert direction
+			this.angle = -this.angle;
+
+		}
+
+		if ((this.y) <= 0) {
+			this.y = (10);
+			this.velocity_y = -this.velocity_y;
+
+			//went out from top
+			//release the key
+			keys[38] = false;
+			//invert direction
+			this.angle = 180;
+		}
+		else if ((this.y + this.height) >= canvas.height) {
+			this.y = (canvas.height - this.height) - 10;
+			this.velocity_y = 0;
+			//went out from bottom
+			//release the key
+			keys[40] = false;
+			//invert direction
+			this.angle = 0;
+		}
+	}
+
+	get getVelocityX()
+	{
+		return this.velocity_x;
+	}
+
+	get getVelocityY()
+	{
+		return this.velocity_y;
+	}
+
+	get getLastX()
+	{
+		return this.last_x;
+	}
+
+	get getLastY()
+	{
+		return this.last_Y;
+	}
+
+
+	setLastPosition(x,y)
+	{
+		this.last_x = x;
+		this.last_y = y;
+	}
+
+	addVelocity(x,y)
+	{
+		this.velocity_x += x;
+		this.velocity_y += y;
+	}
+
+
+    //draw localplayer
+	drawLocalPlayer()
+	{
+					
+		var canvas = document.getElementById('canvas');			
+		var ctx = canvas.getContext('2d');
+
+		//set wanted text values
+		ctx.fillStyle = 'rgba(255, 255, 23)';
+		ctx.font = '18px tahoma';
+		ctx.textAlign = 'center';		
+		var center_x = (this.x + (this.width / 2));
+		//draw text on center of this objects size. only on x axis
+		ctx.fillText('(you)', center_x, this.y - 20);
+
+		//save canvas current state
+		ctx.save();
+		//change colour
+		ctx.fillStyle = 'rgb(255, 255, 0)';
+
+		//translate the position
+		ctx.translate(this.x+this.width/2,this.y+this.height/2);
+
+		//rotate the shape in degrees
+		ctx.rotate(this.angle*Math.PI/180);
+
+		//set color based on role
+		if(this.role == 'poliisi')
+		{
+			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+		}
+		else
+		{
+			ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
+		}
+
+		//draw the base rectangle
+		ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
+
+		ctx.fillStyle = 'rgb(200, 200, 200)';
+
+		//draw gray rect to indicate the current move direction
+		ctx.fillRect(-this.width/2, -this.height/2, this.width, 5);
+
+		//restore the original canvas state
+		ctx.restore();
+	
+		//size of each slice of the image
+		var step_width = 62;
+		var step_height = 62;
+	
+		ctx.drawImage(img,horizontal_index  * step_width, vertical_index * step_height,
+			step_width, step_height, this.x, this.y,step_width,step_height);
+
+		counter++;
+
+		if(counter > 5)
+		{
+			//next slice
+			horizontal_index++;
+			//4 slices per horizontal and vertical row
+			if (horizontal_index >= 4) {
+				horizontal_index = 0;
+
+				//move to next row
+				vertical_index++;
+
+				if(vertical_index >= 4)
+				{
+					vertical_index = 0;
+				}
+				
+			}
+
+			counter = 0;
+		}		
+	}
+
+	setLastAngle(angle)
+	{
+		this.last_angle = angle;
+	}
+
+    //check if localplayer has moved or changed angle and network that to the server
+	checkSendUpdate() {
+
+		//convert to int to prevent unnecessary updates saving network bandwidth
+		var x_int = Math.floor(this.x);
+		var y_int = Math.floor(this.y);
+		var angle_int = Math.floor(this.angle);
+
+		var lastx_int = Math.floor(this.last_x);
+		var lasty_int = Math.floor(this.last_y);
+		var last_angle_int = Math.floor(this.last_angle);
+
+
+		//check comparing previous values if we need to send update to server
+		if (x_int != lastx_int || y_int != lasty_int || angle_int != last_angle_int) {
+
+			//emit event to server of our position/angle update
+			socket.emit('position update', this.x, this.y, this.angle);			
+			//update the latest sent info
+			this.setLastPosition(x_int,y_int);
+			this.setLastAngle(angle_int);
+		}
+	}
+
+	setPosition(x,y)
+	{	
+		super.setPosition(x,y);
+		this.handleBounds();
+		this.checkSendUpdate();
+	}
+
+
+}
+
+class Enemy extends BasePlayer
+{
+	constructor(x,y)
+	{
+		super(x,y);
+	}
+
+    //draw networked player
+	drawEnemy()
+	{
+					
+		var canvas = document.getElementById('canvas');			
+		var ctx = canvas.getContext('2d');
+
+
+		ctx.save();
+		ctx.fillStyle = 'rgb(255, 255, 0)';
+
+		ctx.translate(this.x+this.width/2,this.y+this.height/2);
+
+		ctx.rotate(this.angle*Math.PI/180);
+
+		if(this.role == 'poliisi')
+		{
+			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+		}
+		else
+		{
+			ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
+
+		}
+		//base draw
+		ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
+
+		ctx.fillStyle = 'rgb(200, 200, 200)';
+
+		//show which way its facing
+		ctx.fillRect(-this.width/2, -this.height/2, this.width, 5);
+
+		ctx.restore();
+
+	}
+
+}
